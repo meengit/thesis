839ed5117611b5dbb8d99dda09a486ef3d2a6b33
Andreas <development@meen.ch>
2020-01-11 12:43:45 +0100

    NEW: PowerShell Connection Tests

---
+++ exponat/helpers/ps1/post.ps1
@@ -0,0 +1,12 @@
+Param (
+  [String]$p="---"
+)
+
+$d = $p
+$r = Invoke-WebRequest https://typewriter.meen.ch/post -Method 'POST' -Headers @{'Content-Type' = 'text/plain'} -Body $d
+
+Write-Host "`nStatus: $($r.StatusCode)"
+Write-Host "`nBody (if available):`n$($r.Content)"
+Write-Host "`n"
+$r.Headers
+
