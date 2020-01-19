5c76e47703119936de2873b2d86c600c05107c2e
Andreas <development@meen.ch>
2020-01-19 17:12:10 +0100

    ADD: Delete handler

--- exponat/routes/post.js
+++ exponat/routes/post.js
@@ -7,9 +7,27 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   var data = req.body;
   var file = path.join(process.cwd(), 'data', 'text.txt');
-  fs.appendFile(file, data, (err) => {
-    if (err) throw err;
-  });
+
+  if ((data === "#") || (data === "\#") || (data === "\\#")) {
+    fs.readFile(file, 'utf8', (err, d) => {
+      if (err) throw err;
+
+      if (d.substr(d.length -4) === "\<br\>") {
+        data = d.substr(0, d.length -4);
+      } else {
+        data = d.substr(0, d.length -1);
+      }
+
+      fs.writeFile(file, data, 'utf8', (err) => {
+        if (err) throw err;
+      });
+    });
+  }
+  else {
+    fs.appendFile(file, data, (err) => {
+      if (err) throw err;
+    });
+  }
 
   res.type('text/plain');
   res.send(data);
